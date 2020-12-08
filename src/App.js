37c16353cb569fcdeed0ed0e202c4b5ac1917b77
eduricharte15 <edu.richarte@gmail.com>
2020-12-08 15:40:58 -0700

    Added some example text to init with the gallery

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-4xl">Hola Mundo</h1>
+    <div className="App ">
+      <div className="bg-gray-800">
+        <h1 className="text-4xl font-bold">Hola Mundo</h1>
+        <p className="text-gray-50">Esta es una prueba de React Js</p>
+      </div>
+      <div className="h-screen bg-red-400 text-center">
+        <h1 className="text-8xl">Bienvenidos a mi galería</h1>
+      </div>
     </div>
   );
 }
