05db61b0a0693042f337a025d25b0b66f6c5ec9a
eduricharte15 <edu.richarte@gmail.com>
2020-12-08 20:45:13 -0700

    added functionality, style and pixabay API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/imageCard";
+import ImageSearch from "./components/imageSearch";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
-    <div className="App ">
-      <div className="bg-gray-800">
-        <h1 className="text-4xl font-bold">Hola Mundo</h1>
-        <p className="text-gray-50">Esta es una prueba de React Js</p>
-      </div>
-      <div className="h-screen bg-red-400 text-center">
-        <h1 className="text-8xl">Bienvenidos a mi galería</h1>
-        <div className="text-black font-bold text-left">
-          <p>En esta sección aparecerán las fotos</p>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No images found :c</h1>}
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 }
